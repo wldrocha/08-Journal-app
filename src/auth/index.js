1e9e0629aa53d657f722a4af2deef88626d83995
Wladimir Rocha <wld.rocha@gmail.com>
2022-08-04 19:35:09 -0400

    [ADD] layout

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,3 +1,3 @@
-// export * from './layout'
+export * from './layout'
 export * from './pages'
 export * from './routes'
---
+++ src/auth/layout/AuthLayout.jsx
@@ -0,0 +1,30 @@
+import { Grid, Typography } from '@mui/material'
+
+export const AuthLayout = ({ children, title = '' }) => {
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction='column'
+      alignItems='center'
+      justifyContent='center'
+      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
+    >
+      <Grid
+        item
+        xs={3}
+        sx={{
+          width: { md: '450px' },
+          backgroundColor: 'white',
+          padding: 3,
+          borderRadius: 2
+        }}
+      >
+        <Typography variant='h5' sx={{ mb: 1 }}>
+          {title}
+        </Typography>
+        {children}
+      </Grid>
+    </Grid>
+  )
+}
