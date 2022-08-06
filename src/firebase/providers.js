92aa31d1c69a23498a7aaafaf24dd4bdeaa06625
Wladimir Rocha <wld.rocha@gmail.com>
2022-08-06 19:23:53 -0400

    [ADD] google sign in

---
+++ src/firebase/providers.js
@@ -0,0 +1,27 @@
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { FirebaseAuth } from '.'
+
+const googleProvider = new GoogleAuthProvider()
+
+export const signInWithGoogle = async () => {
+  try {
+    const result = await signInWithPopup(FirebaseAuth, googleProvider)
+    // const credentials = GoogleAuthProvider.credentialFromResult(result)
+    const { displayName, email, photoURL, uid } = result.user
+    return {
+      success: true,
+      displayName,
+      email,
+      photoURL,
+      uid
+    }
+  } catch (error) {
+    
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    return{
+      success: false,
+      errorMessage
+    }
+  }
+}
--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,3 +1,4 @@
+import { signInWithGoogle } from '../../firebase/providers'
 import { checkingCredentials } from './'
 
 export const checkingAuthentication = ({ email, password }) => {
@@ -9,5 +10,8 @@ export const checkingAuthentication = ({ email, password }) => {
 export const startGoogleSignIn = () => {
   return async (dispatch) => {
     dispatch(checkingCredentials())
+    const result = await signInWithGoogle()
+    
+
   }
 }
