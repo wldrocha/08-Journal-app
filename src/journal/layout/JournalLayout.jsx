7a560c4afe033df1976d25d787a35715d7dfd174
Wladimir Rocha <wld.rocha@gmail.com>
2022-08-06 12:40:05 -0400

    [ADD] sidebar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,14 +1,16 @@
+import { Toolbar } from '@mui/material'
 import { Box } from '@mui/system'
-import { Navbar } from '../components'
+import { Navbar, SideBar } from '../components'
 
 const drawerWidth = 240
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex' }}>
-      <Navbar drawerWidth={drawerWidth} />
-      {/* Sidebar */}
+      <Navbar {...drawerWidth} />
+      <SideBar {...drawerWidth} />
       <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
         {children}
       </Box>
     </Box>
