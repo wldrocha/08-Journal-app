d3cbabb904d8783fcc6348463fb13cc4e77c12f7
Wladimir Rocha <wld.rocha@gmail.com>
2022-08-06 13:39:57 -0400

    [ADD] button

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,6 @@
-import { Typography } from '@mui/material'
+import { IconButton } from '@mui/material'
 import { JournalLayout } from '../'
+import { AddOutlined } from '@mui/icons-material'
 import { NothingSelectedView, NoteView } from '../views'
 
 export const JournalPage = () => {
@@ -7,6 +8,19 @@ export const JournalPage = () => {
     <JournalLayout>
       {/* <NothingSelectedView /> */}
       <NoteView />
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'secondary.main',
+          ':hover': { backgroundColor: 'secondary.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   )
 }
