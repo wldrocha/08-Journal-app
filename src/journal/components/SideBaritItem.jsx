749736288b7f520d0d9e5e8dad565f1bc1c4d107
Wladimir Rocha <wld.rocha@gmail.com>
2022-08-13 13:27:01 -0400

    [ADD] set activeNote

--- src/journal/components/SideBaritItem.jsx
+++ src/journal/components/SideBaritItem.jsx
@@ -1,12 +1,20 @@
 import { useMemo } from 'react'
+import { useDispatch } from 'react-redux'
 import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import { TurnedInNot } from '@mui/icons-material'
+import { setActiveNote } from '../../store/journal'
 
-const SideBaritItem = ({ id, title = '', body }) => {
+const SideBaritItem = ({ id, title = '', body, date, imageUrls = [] }) => {
+  const dispatch = useDispatch()
+
   const newTitle = useMemo(() => (title.length > 17 ? title.substring(0, 17) + '...' : title), [title])
 
+  const onActivateNote = () => {
+    dispatch(setActiveNote({ id, title, body, date, imageUrls }))
+  }
+
   return (
-    <ListItem disablePadding>
+    <ListItem disablePadding onClick={() => onActivateNote()}>
       <ListItemButton>
         <ListItemIcon>
           <TurnedInNot />
