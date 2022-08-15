5bc8d1f1d8dc6db7c5400386431605fdb7fab812
Wladimir Rocha <wld.rocha@gmail.com>
2022-08-15 16:47:52 -0400

    [ADD] base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: './dist', 
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'nested/index.html')
+      }
+    }
+  }
 })
