c5bea9b0157f7d19efda8c40b7b3a4bcf1ce27fe
Wladimir Rocha <wld.rocha@gmail.com>
2022-08-15 16:51:14 -0400

    [DEL] build options

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './dist', 
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'nested/index.html')
-      }
-    }
-  }
+  base: './dist'
 })
