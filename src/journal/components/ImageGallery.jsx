c1a5f25dd40cf7f889c214ea81ab887c74a1d223
Wladimir Rocha <wld.rocha@gmail.com>
2022-08-15 16:14:14 -0400

    [REF] validate array images

--- src/journal/components/ImageGallery.jsx
+++ src/journal/components/ImageGallery.jsx
@@ -1,7 +1,7 @@
 import ImageList from '@mui/material/ImageList'
 import ImageListItem from '@mui/material/ImageListItem'
 
-export const ImageGallery = ({images}) => {
+export const ImageGallery = ({images = []}) => {
   return (
     <ImageList sx={{ width: '100%', height: 500 }} cols={4} rowHeight={200}>
       {images.map((image) => (
