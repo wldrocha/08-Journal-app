2bb01bb85eaf64ce1c1094cfa5f4f66c906d2717
Wladimir Rocha <wld.rocha@gmail.com>
2022-08-16 16:38:29 -0400

    [ADD] BASE URL

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://wldrocha.github.io/Journal-app/'
+  base: '/journal-app/'
 })
