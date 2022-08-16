d34ca764801c39aa6f1c85624c0c0a2b8cbcf185
Wladimir Rocha <wld.rocha@gmail.com>
2022-08-16 18:22:24 -0400

    [ADD] basename

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { JournalApp } from './JournalApp'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename='/'>
         <JournalApp />
       </BrowserRouter>
     </Provider>
