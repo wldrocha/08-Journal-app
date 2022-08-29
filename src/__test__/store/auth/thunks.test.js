3863926e62e94ce31503d724bd36be5b333e10af
Wladimir Rocha <wld.rocha@gmail.com>
2022-08-29 10:03:59 -0400

    [ADD[ startGoogleSingIn

--- src/__test__/store/auth/thunks.test.js
+++ src/__test__/store/auth/thunks.test.js
@@ -1,5 +1,7 @@
-import { checkingCredentials } from '../../../store/auth'
-import { checkingAuthentication } from '../../../store/auth/thunks'
+import { signInWithGoogle } from '../../../firebase/providers'
+import { checkingCredentials, login, logout } from '../../../store/auth'
+import { checkingAuthentication, startGoogleSignIn } from '../../../store/auth/thunks'
+import { demoUser } from '../../authFixtures/authFixtures'
 
 jest.mock('../../../firebase/providers')
 
@@ -11,5 +13,31 @@ describe('authThunks test', () => {
     await checkingAuthentication()(dispatch)
 
     expect(dispatch).toHaveBeenCalledWith(checkingCredentials())
+  })
+
+  test(' startGoogleSignIn should to call to checkingCredentials and login', async () => {
+    const loginData = { success: true, ...demoUser }
+
+    //mock for popup
+    await signInWithGoogle.mockResolvedValue(loginData)
+
+    await startGoogleSignIn()(dispatch)
+    //evaluate thunk
+    expect(dispatch).toHaveBeenCalledWith(checkingCredentials())
+
+    expect(dispatch).toHaveBeenCalledWith(login(loginData))
+  })
+
+  test('startGoogleSignIn should to call to checkingCredentials and logout error', async () => {
+    const loginData = { success: false, errorMessage: 'Hubo un error' }
+
+    //mock for popup
+    await signInWithGoogle.mockResolvedValue(loginData)
+
+    await startGoogleSignIn()(dispatch)
+    //evaluate thunk
+    expect(dispatch).toHaveBeenCalledWith(checkingCredentials())
+
+    expect(dispatch).toHaveBeenCalledWith(logout(loginData.errorMessage))
   })
 })
