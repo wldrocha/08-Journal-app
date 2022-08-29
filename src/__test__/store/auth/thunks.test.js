70dd386b32edf82ea93e61b3f40c215436483418
Wladimir Rocha <wld.rocha@gmail.com>
2022-08-29 14:01:29 -0400

    [ADD] rest test

--- src/__test__/store/auth/thunks.test.js
+++ src/__test__/store/auth/thunks.test.js
@@ -1,6 +1,18 @@
-import { signInWithGoogle } from '../../../firebase/providers'
-import { checkingCredentials, login, logout } from '../../../store/auth'
-import { checkingAuthentication, startGoogleSignIn } from '../../../store/auth/thunks'
+import {
+  loginWithEmailPassword,
+  logoutFirebase,
+  signInWithGoogle,
+  registerUserWithEmailPassword
+} from '../../../firebase/providers'
+import { checkingCredentials, login, logout } from '../../../store/auth/authSlice'
+import {
+  checkingAuthentication,
+  startGoogleSignIn,
+  startCreatingUserWithEmailPassword,
+  startLoginWithEmailPassword,
+  startLogout
+} from '../../../store/auth/thunks'
+import { clearNotesLogout } from '../../../store/journal/journalSlice'
 import { demoUser } from '../../authFixtures/authFixtures'
 
 jest.mock('../../../firebase/providers')
@@ -15,7 +27,7 @@ describe('authThunks test', () => {
     expect(dispatch).toHaveBeenCalledWith(checkingCredentials())
   })
 
-  test(' startGoogleSignIn should to call to checkingCredentials and login', async () => {
+  test('startGoogleSignIn should to call to checkingCredentials and login', async () => {
     const loginData = { success: true, ...demoUser }
 
     //mock for popup
@@ -39,5 +51,70 @@ describe('authThunks test', () => {
     expect(dispatch).toHaveBeenCalledWith(checkingCredentials())
 
     expect(dispatch).toHaveBeenCalledWith(logout(loginData.errorMessage))
+  })
+
+  test('startCreatingUserWithEmailPassword should to call login - success', async () => {
+    const loginData = { success: true, ...demoUser, password: '123456' }
+
+    // importar y declarar la función interna a mockear
+    await registerUserWithEmailPassword.mockResolvedValue(loginData)
+    // luego llamar a la función del thunk con sus parametros y dispatch
+    await startCreatingUserWithEmailPassword(loginData)(dispatch)
+    //verificar que el dispatch llame a las funciones respectivas
+    expect(dispatch).toHaveBeenCalledWith(checkingCredentials())
+
+    expect(dispatch).toHaveBeenCalledWith(login(loginData))
+  })
+
+  test('startCreatingUserWithEmailPassword should to call to checkingCredentials and logout error', async () => {
+    const loginData = { success: false, errorMessage: 'Hubo un error' }
+
+    const formData = { email: demoUser.email, password: '123456' }
+
+    // importar y declarar la función interna a mockear
+    await registerUserWithEmailPassword.mockResolvedValue(formData)
+    // luego llamar a la función del thunk con sus parametros y dispatch
+    await startCreatingUserWithEmailPassword(formData)(dispatch)
+    //verificar que el dispatch llame a las funciones respectivas
+    expect(dispatch).toHaveBeenCalledWith(checkingCredentials())
+
+    expect(dispatch).toHaveBeenCalledWith(logout())
+  })
+
+  test('startLoginWithEmailPassword should to call to checkingCredentials and login - success', async () => {
+    const loginData = { success: false, errorMessage: 'Hubo un error', ...demoUser }
+    const formData = { email: demoUser.email, password: '123456' }
+
+    await loginWithEmailPassword.mockResolvedValue(loginData)
+
+    await startLoginWithEmailPassword(formData)(dispatch)
+
+    //evaluate thunk
+    expect(dispatch).toHaveBeenCalledWith(checkingCredentials())
+
+    expect(dispatch).toHaveBeenCalledWith(logout(loginData))
+  })
+
+  test('startLoginWithEmailPassword should to call to checkingCredentials and login - Error', async () => {
+    const loginData = { success: false, errorMessage: 'Hubo un error' }
+
+    const formData = { email: demoUser.email, password: '123456' }
+
+    //mock for popup
+    await loginWithEmailPassword.mockResolvedValue(loginData)
+
+    await startLoginWithEmailPassword(formData)(dispatch)
+    //evaluate thunk
+    expect(dispatch).toHaveBeenCalledWith(checkingCredentials())
+
+    expect(dispatch).toHaveBeenCalledWith(logout(loginData))
+  })
+
+  test('startLogout should to call to logoutFirebase, clear notes and logout', async () => {
+    await startLogout()(dispatch)
+
+    expect(logoutFirebase).toHaveBeenCalled()
+
+    expect(dispatch).toHaveBeenCalledWith(clearNotesLogout())
   })
 })
